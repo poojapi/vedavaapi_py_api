c14d5f7d1fda133dd978f3cb6cee5d544ccc4750
Sai Susarla <sai.susarla@gmail.com>
2018-07-04 22:20:48 +0530

    Make new run.py and server config the default

--- vedavaapi/run.py
+++ vedavaapi/run.py
@@ -20,9 +20,9 @@ from sanskrit_data.schema.common import JsonObject
 # Add parent directory to PYTHONPATH, so that vedavaapi_py_api module can be found.
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from vedavaapi_py_api import common, textract, ullekhanam
+from vedavaapi import common, ullekhanam
 from sanskrit_data import file_helper
-from vedavaapi_py_api.common.flask_helper import app
+from vedavaapi.common.flask_helper import app
 
 logging.basicConfig(
   level=logging.INFO,
@@ -37,84 +37,65 @@ params.set_from_dict({
   'reset': False
 })
 
+def start_service(name):
+    logging.info("Starting vedavaapi.{} service ...".format(name))
+    svc_cls = "Vedavaapi" + str.capitalize(name)
+    _tmp = __import__('vedavaapi.{}'.format(name), globals(), locals(), [svc_cls], -1)
+    svc_cls = eval('_tmp.'+svc_cls)
+    svc_conf = common.server_config[name] if name in common.server_config else {}
+    svc_obj = svc_cls(name, svc_conf)
+    common.VedavaapiServices.register(name, svc_obj)
+
+    if params.reset:
+        print "Resetting previous state of {} ...".format(name)
+        svc_obj.reset()
+    svc_obj.setup()
+    svc_obj.register_api(app, "/{}".format(name))
 
 def setup_app():
-  common.set_configuration(config_file_name=os.path.join(os.path.dirname(__file__), 'conf_local/server_config.json'))
-  server_config = common.server_config
+    common.set_configuration(config_file_name=os.path.join(os.path.dirname(__file__), 'conf_local/server_config.json'))
 
-  client = None
-  if server_config["db"]["db_type"] == "couchdb":
-    from sanskrit_data.db.implementations import couchdb
-    client = couchdb.CloudantApiClient(url=server_config["db"]["couchdb_host"])
-  elif server_config["db"]["db_type"] == "mongo":
-    from sanskrit_data.db.implementations import mongodb
-    client = mongodb.Client(url=server_config["db"]["mongo_host"])
+    services = ['store', 'users', 'ullekhanam']
 
-  if params.reset:
-    print "Deleting database/collection", server_config["db"]["users_db_name"]
-    client.delete_database(server_config["db"]["users_db_name"])
-    for db_details in server_config["db"]["ullekhanam_dbs"]:
-        print "Deleting database/collection", db_details["backend_id"]
-        client.delete_database(db_details["backend_id"])
-        if "file_store" in db_details:
-            try:
-                os.system("rm -rf " + db_details["file_store"])
-            except Exception as e:
-                logging.error("Error removing " + db_details["file_store"]+": "+ e)
-
-  from vedavaapi_py_api import users
-  users.setup(db=client.get_database_interface(db_name_backend=server_config["db"]["users_db_name"], db_name_frontend="users", db_type="users_db"), initial_users=server_config["initial_users"], default_permissions_in=server_config["default_permissions"])
-
-  # Set up ullekhanam API databases.
-  # ullekhanam is the main database/ service.
-  from vedavaapi_py_api.ullekhanam.backend import add_db
-  for db_details in server_config["db"]["ullekhanam_dbs"]:
-    add_db(db=client.get_database_interface(db_name_backend=db_details["backend_id"],
-    db_name_frontend=db_details["frontend_id"], external_file_store=db_details.get("file_store"), db_type="ullekhanam_db"), reimport=params.reset)
-
-  logging.info("Root path: " + app.root_path)
-  import vedavaapi_py_api.users.api_v1
-  import vedavaapi_py_api.ullekhanam.api_v1
-  import vedavaapi_py_api.textract.api_v1
-  app.register_blueprint(users.api_v1.api_blueprint, url_prefix="/auth")
-  app.register_blueprint(textract.api_v1.api_blueprint, url_prefix="/textract")
-  app.register_blueprint(ullekhanam.api_v1.api_blueprint, url_prefix="/ullekhanam")
+    logging.info("Root path: " + app.root_path)
+    for svc in services:
+        start_service(svc)
 
 def main(argv):
-  def usage():
-    logging.info("run.py [-d] [-r] [--port 4444]...")
-    logging.info("run.py -h")
-    exit(1)
+    def usage():
+        logging.info("run.py [-d] [-r] [--port 4444]...")
+        logging.info("run.py -h")
+        exit(1)
 
-  global params
-  try:
-    opts, args = getopt.getopt(argv, "drp:h", ["port=", "debug="])
-    for opt, arg in opts:
-      if opt == '-h':
+    global params
+    try:
+        opts, args = getopt.getopt(argv, "drp:h", ["port=", "debug="])
+        for opt, arg in opts:
+            if opt == '-h':
+                usage()
+            if opt == '-r':
+                params.reset = True
+            elif opt in ("-p", "--port"):
+                params.port = int(arg)
+            elif opt in ("-d", "--debug"):
+                params.debug = True
+    except getopt.GetoptError:
         usage()
-      if opt == '-r':
-        params.reset = True
-      elif opt in ("-p", "--port"):
-        params.port = int(arg)
-      elif opt in ("-d", "--debug"):
-        params.debug = True
-  except getopt.GetoptError:
-    usage()
 
-  setup_app()
+    setup_app()
 
-  logging.info("Available on the following URLs:")
-  for line in file_helper.run_command(["/sbin/ifconfig"]).split("\n"):
-    import re
-    m = re.match('\s*inet addr:(.*?) .*', line)
-    if m:
-      logging.info("    http://" + m.group(1) + ":" + str(params.port) + "/")
-  app.run(
-    host="0.0.0.0",
-    port=params.port,
-    debug=params.debug,
-    use_reloader=False
-  )
+    logging.info("Available on the following URLs:")
+    for line in file_helper.run_command(["/sbin/ifconfig"]).split("\n"):
+        import re
+        m = re.match('\s*inet addr:(.*?) .*', line)
+        if m:
+            logging.info("    http://" + m.group(1) + ":" + str(params.port) + "/")
+    app.run(
+        host="0.0.0.0",
+        port=params.port,
+        debug=params.debug,
+        use_reloader=False
+    )
 
 
 if __name__ == "__main__":
